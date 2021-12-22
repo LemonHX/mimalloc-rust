f28ea1f523aa69da66c8df48d6849cf2cd8dcc34
lemonhx <lemonhx@lemonhx.tech>
2021-12-22 12:25:56 +0800

    add git submodule
    Co-authored-by: aml360 <aml360esp@gmail.com>
    Signed-off-by: lemonhx <lemonhx@lemonhx.tech>

--- mimalloc-rust-sys/build.rs
+++ mimalloc-rust-sys/build.rs
@@ -1,6 +1,23 @@
-use std::env;
+use std::{env, process::Command};
 
 fn main() {
+    let status = Command::new("git")
+        .args(&["submodule", "update", "--init", "--recursive"])
+        .status();
+    match status {
+        Ok(status) => {
+            if !status.success() {
+                panic!("failed to update git submodule due to {:?}",status);
+            }
+        }
+        Err(err) => {
+            panic!(
+                "failed to clone git submodule of mimalloc due to {:?}",
+                &err
+            )
+        }
+    }
+
     let mut build = cc::Build::new();
 
     build.include("c_src/mimalloc/include");
